07d605329a0974d0538861e3dfd224a5daceb7ba
sean@maplekiwi.com <sean@maplekiwi.com>
2019-06-12 03:08:02 +0000

    Deploy from Forestry.io

--- js/front.js
+++ js/front.js
@@ -22,24 +22,24 @@ $(function () {
   contactForm()
 })
 
-// Ajax contact
-function contactForm () {
-  var form = $('.contact-form')
-  form.submit(function () {
-    $this = $(this)
-    $.post($(this).attr('action'),
-      $this.serialize(),
-      function () {
-        $this[0].reset() // clear form
-
-        $('#contact-message')
-          .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
-          .fadeIn()
-      }
-      , 'json')
-    return false
-  })
-}
+// Ajax contact -- no longer supported by formspree
+// function contactForm () {
+//   var form = $('.contact-form')
+//   form.submit(function () {
+//     $this = $(this)
+//     $.post($(this).attr('action'),
+//       $this.serialize(),
+//       function () {
+//         $this[0].reset() // clear form
+//
+//         $('#contact-message')
+//           .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
+//           .fadeIn()
+//       }
+//       , 'json')
+//     return false
+//   })
+// }
 
 /* for demo purpose only - can be deleted */
 function demo () {
